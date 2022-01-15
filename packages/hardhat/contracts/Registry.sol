37aa61c95a3d359c6e6f01327859d00f71782038
Septaplex <septaplex@protonmail.com>
2022-01-15 16:22:33 +0100

    Implement addVault function

--- packages/hardhat/contracts/Registry.sol
+++ packages/hardhat/contracts/Registry.sol
@@ -1,10 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.11;
 
-contract Registry {
-    bool public isDeployed;
+import { IERC20 } from "./IERC20.sol";
+import { IVault } from "./IVault.sol";
 
-    constructor() {
-        isDeployed = true;
+library Errors {
+    string internal constant _ZeroAddress = "Can't be the zero address";
+    string internal constant _AlreadyAdded = "Vault already added";
+}
+
+contract Registry {
+    struct VaultInfo {
+        uint256 id;
+        IVault vault;
+        IERC20 from;
+        IERC20 to;
+        bool isEntity;
+    }
+
+    uint256 public nextVaultId;
+    mapping(uint256 => VaultInfo) public vaults;
+    mapping(IERC20 => mapping(IERC20 => IVault)) public tokensToVault;
+
+    event AddVault(uint256 indexed id, IVault indexed vault, IERC20 from, IERC20 to);
+
+    function addVault(IVault vault) external returns (uint256) {
+        require(address(vault) != address(0), Errors._ZeroAddress);
+
+        IERC20 from = vault.from();
+        require(address(from) != address(0), Errors._ZeroAddress);
+
+        IERC20 to = vault.to();
+        require(address(to) != address(0), Errors._ZeroAddress);
+
+        IVault existingVault = tokensToVault[from][to];
+        require(address(existingVault) == address(0), Errors._AlreadyAdded);
+
+        uint256 id = nextVaultId;
+        nextVaultId++;
+
+        VaultInfo memory vaultInfo = VaultInfo({ id: id, vault: vault, from: from, to: to, isEntity: true });
+
+        vaults[id] = vaultInfo;
+        tokensToVault[from][to] = vault;
+
+        emit AddVault(id, vault, from, to);
+
+        return id;
     }
 }
--- packages/hardhat/tests/Registry.test.ts
+++ packages/hardhat/tests/Registry.test.ts
@@ -1,20 +1,78 @@
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
+import { ContractFactory } from 'ethers'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 
-import { Registry } from '../types'
+const { AddressZero } = ethers.constants
+
+import { IERC20, MockExchange, Registry, Vault } from '../types'
+
+const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+const WBTC_ADDRESS = '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'
 
 describe('Registry', () => {
+  let to: string
+  let from: string
+  let usdc: IERC20
+  let wbtc: IERC20
+  let vault: Vault
   let registry: Registry
+  let exchange: MockExchange
+  let owner: SignerWithAddress
   let deployer: SignerWithAddress
+  let vaultFactory: ContractFactory
 
   beforeEach(async () => {
-    ;[deployer] = await ethers.getSigners()
-    const factory = await ethers.getContractFactory('Registry', deployer)
-    registry = (await factory.deploy()) as Registry
+    usdc = await ethers.getContractAt('IERC20', USDC_ADDRESS)
+    wbtc = await ethers.getContractAt('IERC20', WBTC_ADDRESS)
+    from = usdc.address
+    to = wbtc.address
+    // Deploy contracts
+    ;[deployer, owner] = await ethers.getSigners()
+    const registryFactory = await ethers.getContractFactory('Registry', deployer)
+    const exchangeFactory = await ethers.getContractFactory('MockExchange', deployer)
+    vaultFactory = await ethers.getContractFactory('Vault', deployer)
+    registry = (await registryFactory.deploy()) as Registry
+    exchange = (await exchangeFactory.deploy()) as MockExchange
+    vault = (await vaultFactory.deploy(from, to, exchange.address)) as Vault
   })
 
-  it('should deploy the contract', async () => {
-    expect(await registry.isDeployed()).to.equal(true)
+  describe('#addVault()', () => {
+    it('should revert when the Vault is the zero address', async () => {
+      const vault = AddressZero
+
+      await expect(registry.connect(owner).addVault(vault)).to.be.revertedWith('the zero address')
+    })
+
+    it('should revert when the Vault\'s "from" token is the zero address', async () => {
+      const from = AddressZero
+      const vault = (await vaultFactory.deploy(from, to, exchange.address)) as Vault
+
+      await expect(registry.connect(owner).addVault(vault.address)).to.be.revertedWith('the zero address')
+    })
+
+    it('should revert when the Vault\'s "to" token is the zero address', async () => {
+      const to = AddressZero
+      const vault = (await vaultFactory.deploy(from, to, exchange.address)) as Vault
+
+      await expect(registry.connect(owner).addVault(vault.address)).to.be.revertedWith('the zero address')
+    })
+
+    it('should revert when the Vault was already added', async () => {
+      await registry.connect(owner).addVault(vault.address)
+      await expect(registry.connect(owner).addVault(vault.address)).to.be.revertedWith('already added')
+    })
+
+    it('should be possible to add a Vault', async () => {
+      const id = ethers.BigNumber.from(0)
+      const isEntity = true
+
+      await expect(registry.connect(owner).addVault(vault.address))
+        .to.emit(registry, 'AddVault')
+        .withArgs(id, vault.address, from, to)
+      expect(await registry.nextVaultId()).to.equal(id.add(1))
+      expect(await registry.vaults(id)).to.deep.equal([id, vault.address, from, to, isEntity])
+      expect(await registry.tokensToVault(from, to)).to.deep.equal(vault.address)
+    })
   })
 })
