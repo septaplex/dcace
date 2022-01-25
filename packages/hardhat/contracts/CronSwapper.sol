737b0979429cdfcc21ce43697979ac7c14e8d71f
Septaplex <septaplex@protonmail.com>
2022-01-25 18:39:44 +0100

    Implement enter function

--- packages/hardhat/contracts/CronSwapper.sol
+++ packages/hardhat/contracts/CronSwapper.sol
@@ -4,10 +4,31 @@ pragma solidity 0.8.11;
 import { IERC20 } from "./interfaces/IERC20.sol";
 import { IExchange } from "./interfaces/IExchange.sol";
 
+library Errors {
+    string internal constant _AmountZero = "Amount can't be 0";
+    string internal constant _DurationZero = "Duration can't be 0";
+}
+
 contract CronSwapper {
+    struct Allocation {
+        uint256 id;
+        uint256 amount;
+        uint256 startDay;
+        uint256 endDay;
+        address owner;
+    }
+
     IERC20 public immutable toSell;
     IERC20 public immutable toBuy;
     IExchange public immutable exchange;
+
+    uint256 public dailyAmount;
+    uint256 public lastExecution;
+    uint256 public nextAllocationId;
+    mapping(uint256 => uint256) public removeAmount;
+    mapping(uint256 => Allocation) public allocations;
+
+    event Enter(uint256 indexed id, address indexed sender, uint256 indexed amount, uint256 startDay, uint256 endDay);
 
     constructor(
         IERC20 toSell_,
@@ -17,5 +38,38 @@ contract CronSwapper {
         toSell = toSell_;
         toBuy = toBuy_;
         exchange = exchange_;
+        lastExecution = _today();
+    }
+
+    function enter(uint256 amount, uint256 duration) external returns (uint256) {
+        require(amount > 0, Errors._AmountZero);
+        require(duration > 0, Errors._DurationZero);
+
+        uint256 total = amount * duration;
+        IERC20(toSell).transferFrom(msg.sender, address(this), total);
+
+        uint256 startDay = _today();
+
+        if (lastExecution == startDay) {
+            startDay += 1;
+        }
+
+        uint256 endDay = startDay + duration;
+        dailyAmount += amount;
+        removeAmount[endDay] += amount;
+
+        uint256 id = nextAllocationId;
+        nextAllocationId++;
+
+        allocations[id] = Allocation({ id: id, amount: amount, startDay: startDay, endDay: endDay, owner: msg.sender });
+
+        emit Enter(id, msg.sender, amount, startDay, endDay);
+
+        return id;
+    }
+
+    function _today() private view returns (uint256) {
+        // solhint-disable-next-line not-rely-on-time
+        return block.timestamp / 1 days;
     }
 }
--- packages/hardhat/tests/CronSwapper.test.ts
+++ packages/hardhat/tests/CronSwapper.test.ts
@@ -1,11 +1,23 @@
 import { expect } from 'chai'
-import { ethers } from 'hardhat'
+import { BigNumber } from 'ethers'
+import { ethers, network } from 'hardhat'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 
 import { IERC20, CronSwapper, MockExchange } from '../types'
 
+const { parseUnits } = ethers.utils
+
 const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
 const WBTC_ADDRESS = '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'
+const WHALE_ADDRESS = '0xE78388b4CE79068e89Bf8aA7f218eF6b9AB0e9d0'
+
+const SECONDS_PER_DAY = 24 * 60 * 60
+
+async function getCurrentDay() {
+  const blockNumber = await ethers.provider.getBlockNumber()
+  const block = await ethers.provider.getBlock(blockNumber)
+  return Math.floor(block.timestamp / SECONDS_PER_DAY)
+}
 
 describe('CronSwapper', () => {
   let usdc: IERC20
@@ -14,6 +26,7 @@ describe('CronSwapper', () => {
   let toBuy: string
   let swapper: CronSwapper
   let exchange: MockExchange
+  let user: SignerWithAddress
   let deployer: SignerWithAddress
 
   beforeEach(async () => {
@@ -21,16 +34,105 @@ describe('CronSwapper', () => {
     wbtc = await ethers.getContractAt('IERC20', WBTC_ADDRESS)
     toSell = usdc.address
     toBuy = wbtc.address
+    // Deploy contracts
     ;[deployer] = await ethers.getSigners()
     const swapperFactory = await ethers.getContractFactory('CronSwapper', deployer)
     const exchangeFactory = await ethers.getContractFactory('MockExchange', deployer)
     exchange = (await exchangeFactory.deploy()) as MockExchange
     swapper = (await swapperFactory.deploy(toSell, toBuy, exchange.address)) as CronSwapper
+    // Impersonate Whale account
+    await network.provider.send('hardhat_impersonateAccount', [WHALE_ADDRESS])
+    user = await ethers.getSigner(WHALE_ADDRESS)
   })
 
   it('should deploy the contract with the provided addresses', async () => {
     expect(await swapper.toSell()).to.equal(toSell)
     expect(await swapper.toBuy()).to.equal(toBuy)
     expect(await swapper.exchange()).to.equal(exchange.address)
+  })
+
+  it('should set the last execution to today when deploying the contract', async () => {
+    const today = await getCurrentDay()
+
+    expect(await swapper.lastExecution()).to.equal(today)
+  })
+
+  describe('#enter()', () => {
+    it('should revert when the amount is 0', async () => {
+      const amount = parseUnits('0', 6)
+      const duration = 7
+
+      await expect(swapper.connect(user).enter(amount, duration)).to.be.revertedWith("Amount can't be 0")
+    })
+
+    it('should revert when the duration is 0', async () => {
+      const amount = parseUnits('100', 6)
+      const duration = 0
+
+      await expect(swapper.connect(user).enter(amount, duration)).to.be.revertedWith("Duration can't be 0")
+    })
+
+    it('should be possible to enter and create an allocation', async () => {
+      // Contract deployment was today --> time-travel to one day in the future
+      await network.provider.send('evm_increaseTime', [SECONDS_PER_DAY])
+      await network.provider.send('evm_mine')
+
+      const today = await getCurrentDay()
+
+      const amount = parseUnits('100', 6)
+      const duration = 7
+
+      const total = amount.mul(duration)
+      await usdc.connect(user).approve(swapper.address, total)
+
+      const startDay = today
+      const endDay = startDay + duration
+
+      await expect(swapper.connect(user).enter(amount, duration))
+        .to.emit(swapper, 'Enter')
+        .withArgs(0, user.address, amount, startDay, endDay)
+
+      expect(await usdc.balanceOf(swapper.address)).to.equal(total)
+      expect(await swapper.dailyAmount()).to.equal(amount)
+      expect(await swapper.removeAmount(endDay)).to.equal(amount)
+      expect(await swapper.nextAllocationId()).to.equal(1)
+      expect(await swapper.allocations(0)).to.deep.equal([
+        BigNumber.from(0),
+        amount,
+        BigNumber.from(startDay),
+        BigNumber.from(endDay),
+        user.address
+      ])
+    })
+
+    it('should increase the start day by one if the last execution was today', async () => {
+      // Contract deployment was today --> time of last execution is today
+      const today = await getCurrentDay()
+
+      const amount = parseUnits('100', 6)
+      const duration = 7
+
+      const total = amount.mul(duration)
+      await usdc.connect(user).approve(swapper.address, total)
+
+      const startDay = today + 1
+      const endDay = startDay + duration
+
+      await expect(swapper.connect(user).enter(amount, duration))
+        .to.emit(swapper, 'Enter')
+        .withArgs(0, user.address, amount, startDay, endDay)
+
+      expect(await usdc.balanceOf(swapper.address)).to.equal(total)
+      expect(await swapper.dailyAmount()).to.equal(amount)
+      expect(await swapper.removeAmount(endDay)).to.equal(amount)
+      expect(await swapper.nextAllocationId()).to.equal(1)
+      expect(await swapper.allocations(0)).to.deep.equal([
+        BigNumber.from(0),
+        amount,
+        BigNumber.from(startDay),
+        BigNumber.from(endDay),
+        user.address
+      ])
+    })
   })
 })
